91194d00f207144bf06b8dc638586e68a11fe166
You-Sheng Yang <vicamo@gmail.com>
2018-01-04 11:59:32 +0800

    fix extra new line generated by update.sh

--- update.sh
+++ update.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-for dockerfile in $(find . -type f -name Dockerfile); do
+travisEnv=
+for dockerfile in $(find . -type f -name Dockerfile | sort); do
   dockerfile=${dockerfile#./}
   suite=${dockerfile%/Dockerfile}
   test -n "$(grep '^# GENERATED' ${dockerfile})" || continue;
   cat Dockerfile.template | sed -e "s!@SUITE@!${suite}!g" > ${dockerfile}
+
+  travisEnv+='  - SUITE='"${suite}"'\n'
 done
+travisEnv=${travisEnv%'\n'}
+
+travis="$(awk -v 'RS=\n\n' '($1 == "env:") { $0 = substr($0, 0, index($0, "matrix:") + length("matrix:"))"'"$travisEnv"'" } { printf "%s%s", $0, RS }' .travis.yml)"
+echo "$travis" > .travis.yml
